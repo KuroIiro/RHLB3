54e83c7ad30a3d98ba37281dea4bf3e09db64ab6
KuroIiro <e2058206@soka-u.jp>
2021-02-28 19:02:25 +0900

    Init: project2

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.send('Hello world!');
+});
+
+/*app.listen(3000,function(){
+
+});*/
+(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
