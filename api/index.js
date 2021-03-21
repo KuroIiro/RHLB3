b595e058835caac4bc0e34d8d2937c40ca94b383
KuroIiro <e2058206@soka-u.jp>
2021-03-21 17:37:24 +0900

    test

--- api/index.js
+++ api/index.js
@@ -2,11 +2,22 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/',function(req,res){
     //res.send('Hello world!');
-    res.json();
+    res.json({id:"kusa"});
 });
+
+app.get('/api/get', function (req,res) {
+    res.send('Hello');
+ });
+
+ app.post('/api/post', function (req,res) {
+    res.send('Heo');
+    console.log(req.body);
+ });
 
 /*app.listen(3000,function(){
 
