6c51de093784218ca98877b5c4bf376b45e8d462
KuroIiro <e2058206@soka-u.jp>
2021-06-06 13:52:14 +0900

    test

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const { response } = require('express');
 const express = require('express');
+const request = require('request');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+const accessToken = "7gIxUaaT/mK/Ueb8tbfLYswh1UJGIPRoQ5/GvzDHCIMqsx8zg9lOuSvEytX3T965MmrlN5nIeTqL4Flgka4LvziVBkjHSYFjJBcq/xCeZfQl0oZaK/11sTaSmbeerZNf6xH7BmswwQIwMu8orwVUpQdB04t89/1O/w1cDnyilFU=";
 
 /*app.get('/',function(req,res){
     //res.send('Hello world!');
@@ -21,15 +23,71 @@ app.get('/', function (req,res) {
  });*/
 
  app.post('/',function(req,res){
+   res.send('api:OK');
     const data = req.body;
     console.log('req.body',data);
 
     const messageId = req.body['events'][0]['message']['id'];
     console.log(messageId);
 
-    res.send('api:OK');
+    const options = {
+         url: `https://api-data.line.me/v2/bot/message/${req.body.events[0].message.id}/content`,
+         method: 'get',
+         headers: {
+            'Authorization': 'Bearer ' + accessToken,
+         },
+         encoding: null
+      };
+
+      request(options, function(error, response, body) {
+      const buffer = new Buffer.from(body);
+      console.log(buffer);
+      
+         const options = {
+            uri: "https://leadhack-test.cognitiveservices.azure.com/customvision/v3.0/Prediction/81b2aff8-0aa1-42c3-b7e0-7eca4ca5a7dd/classify/iterations/O-dori-/image",
+            headers: {
+               "Content-type": "application/octet-stream",
+               "Prediction-Key": "1f0cd8f7237b4d598420ff1ab62ba8cc",
+            },
+            body: buffer,
+         };
+
+         request.post(options, function(error, response, body){
+            console.log(body);
+            request.post(optionsLine, function(error, res, body){
+               const resBody = JSON.parse(body);
+
+               const messageData = {
+                  "replyToken" : replyToken,
+                  "messages":[
+                     {
+                        "type":"text",
+                        "text":"Message Body"
+                     }
+                  ]
+               }
+
+               const optionsLine = {
+                  uri: 'https://api.line.me/v2/bot/message/reply',
+                  headers: {
+                     'Content-Type':'applucation/json',
+                     'Authorization': 'Bearer' + accessToken,
+                  },
+                  json: messageData
+               }
+               request.post(optionsLine, function(error, res, body){});
+
+            });
+            
+         });
+      });
 
  });
+
+
+
+
+
 
 /*app.listen(3000,function(){
 
